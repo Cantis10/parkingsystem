a368bb09446f7807c092447cbd7f780750d199b7
Cantis <148445784+Cantis10@users.noreply.github.com>
2025-10-16 15:25:35 +0000

    sh

--- public/api/home.js
+++
@@ -1,9 +0,0 @@
-// /api/login.js
-import path from 'path';
-import fs from 'fs';
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'public', 'home.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.status(200).send(html);
-}
